5ea139aa7c6ceca39b6a5fa80e54e939a28c0181
Seferan <seferan@gmail.com>
2018-07-09 08:48:22 -0500

    Improve Formatting of Code.  No Functional Changes

--- src/main/java/me/seferan/pressbutton/action/ScriptActionPressButton.java
+++ src/main/java/me/seferan/pressbutton/action/ScriptActionPressButton.java
@@ -18,52 +18,47 @@ import net.minecraft.client.gui.GuiScreen;
 @APIVersion(ModuleInfo.API_VERSION)
 public class ScriptActionPressButton extends ScriptAction {
 
-	public ScriptActionPressButton()
-	{
-		super(ScriptContext.MAIN, "pressbutton");		
+	public ScriptActionPressButton() {
+		super(ScriptContext.MAIN, "pressbutton");
 	}
-	
-	public void onInit()
-	{
+
+	public void onInit() {
 		this.context.getCore().registerScriptAction(this);
 	}
-	
-	public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams, String[] params){
-		
-	    if (params.length > 0) {
-	        try
-	        {
-	        	
-	          int id = ScriptCore.tryParseInt(provider.expand(macro, params[0], false), 0);	          
-	          
-	          int L = 0;int R = 1;
-	          int button = 0;
-	          if (params.length > 1)
-	          {
-	            String leftOrRight = ScriptCore.parseVars(provider, macro, params[1], false);
-	            
-	            button = leftOrRight.startsWith("l") ? 0 : 1;
-	          }
-	          GuiScreen open = Minecraft.getMinecraft().currentScreen;
-	          if (open != null)
-	          {
-	            int doButtonClick = Access.doButtonClick(open, id, button);
-	            if (doButtonClick == Integer.MAX_VALUE) {
-	              Log.info("Invalid button ID " + id, provider);
-	            } else if (doButtonClick != -1) {
-	              if (doButtonClick == -2) {
-	            	  Log.info("Error in creation of reflection, please report the logs to the author", provider);
-	              } else {
-	                return new ReturnValue(doButtonClick == 0);
-	              }
-	            }
-	          }
-	        }
-	        catch (NumberFormatException notInt)
-	        {
-	        	Log.info(params[0] + " is not an int", provider);
-	        }
-	    }
-	    return new ReturnValue(false);	
+
+	public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams,
+			String[] params) {
+
+		if (params.length > 0) {
+			try {
+
+				int id = ScriptCore.tryParseInt(provider.expand(macro, params[0], false), 0);
+
+				int L = 0;
+				int R = 1;
+				int button = 0;
+				if (params.length > 1) {
+					String leftOrRight = ScriptCore.parseVars(provider, macro, params[1], false);
+
+					button = leftOrRight.startsWith("l") ? 0 : 1;
+				}
+				GuiScreen open = Minecraft.getMinecraft().currentScreen;
+				if (open != null) {
+					int doButtonClick = Access.doButtonClick(open, id, button);
+					if (doButtonClick == Integer.MAX_VALUE) {
+						Log.info("Invalid button ID " + id, provider);
+					} else if (doButtonClick != -1) {
+						if (doButtonClick == -2) {
+							Log.info("Error in creation of reflection, please report the logs to the author", provider);
+						} else {
+							return new ReturnValue(doButtonClick == 0);
+						}
+					}
+				}
+			} catch (NumberFormatException notInt) {
+				Log.info(params[0] + " is not an int", provider);
+			}
+		}
+		return new ReturnValue(false);
 	}
 }
--- src/main/java/me/seferan/pressbutton/util/ModuleInfo.java
+++ src/main/java/me/seferan/pressbutton/util/ModuleInfo.java
@@ -4,15 +4,16 @@ package me.seferan.pressbutton.util;
  * Shared information for this module
  */
 public abstract class ModuleInfo {
-    
-    /**
-     * Target API version
-     */
-    public static final int API_VERSION = 26;
 
-    /**
-     * Private ctor, no instances 
-     */
-    private ModuleInfo() {}
+	/**
+	 * Target API version
+	 */
+	public static final int API_VERSION = 26;
+
+	/**
+	 * Private ctor, no instances
+	 */
+	private ModuleInfo() {
+	}
 
 }
